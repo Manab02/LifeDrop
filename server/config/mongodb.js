f44e3a78c96724202ebc5a3c67197c39b04fe68c
Manab02 <dhuamanab08@gmail.com>
2026-04-12 22:24:23 +0530

    modified:   server/config/mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -4,7 +4,6 @@ const connetDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database Connected"));    //()=> is call back function called 
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/life-drop`);
+    await mongoose.connect(process.env.MONGODB_URI);;
 };
-
 export default connetDB;
